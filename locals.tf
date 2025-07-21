853ad715aab2aca23038b9dd9c7b6c3c94b26ea7
Sai Srinivas Idimadakala <saisrinivas.idimadakala@gmail.com>
2025-07-21 09:56:33 +0530

    added component module

--- locals.tf
+++ locals.tf
@@ -2,10 +2,7 @@ locals {
   ami_id = data.aws_ami.catalogue.id
   vpc_id = data.aws_ssm_parameter.vpc_id.value
   roboshop_private_subnet_ids = split(",",data.aws_ssm_parameter.roboshop_private_subnet_ids.value)
-  #backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
-  #catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
-  
   frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
   sg_id = data.aws_ssm_parameter.sg_id.value
   alb_listener_arn = "${var.component}" == "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
